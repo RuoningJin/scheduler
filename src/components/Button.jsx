a3f63cb9df6f6e56329e8ee45a9760532d415173
RuoningJin <ronjin2013@hotmail.com>
2022-11-29 15:55:22 -0500

    conditions added to the button to render different styles

---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import classNames from "classnames";
+import "components/Button.scss";
+
+export default function Button(props) {
+  const buttonClass = classNames('button', {
+    'button--confirm': props.confirm, 
+    'button--danger': props.danger
+  });
+
+  return <button disabled={props.disabled} onClick={props.onClick} className={buttonClass}>{props.children}</button>;
+}
